ae964e23ab9e2329836c5692c106eddc55e6a2da
marsel1996 <marselmustafin19966@mail.ru>
2018-01-21 18:45:31 +0500

    м риотольд

---
+++ RealtorsSite/Realtors/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Realtors.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult ContactView()
+        {
+            return View();
+        }
+    }
+}
